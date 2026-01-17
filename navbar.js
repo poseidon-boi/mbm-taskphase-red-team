376447c3bbca37bcd63843083f0bf02f2346d9ee
gayatrimudaliar592-cell <gayatrimudaliar592@gmail.com>
2026-01-17 18:10:57 +0530

    Biosafety content

--- navbar.js
+++ navbar.js
@@ -3,12 +3,12 @@ const links = document.getElementById("links");
 const ddown_open = document.querySelectorAll(".ddown");
 
 hamburger.addEventListener("click", () => {
-    links.classList.toggle("active");
+  links.classList.toggle("active");
 });
 
-ddown_open.forEach(button => {
-    button.addEventListener("click",(e) => {
-        e.stopPropagation();
+ddown_open.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.stopPropagation();
     button.classList.toggle("open");
-});
+  });
 });
